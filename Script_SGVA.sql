614f388720892ad1d93dbe39ab9be732b83a9516
Hedigu <hedigu290803@gmail.com>
2026-04-06 16:26:26 +0000

    Cambios

--- Script_SGVA.sql
+++ Script_SGVA.sql
@@ -3,13 +3,14 @@ CREATE DATABASE Trazabilidad_SGVA;
 USE Trazabilidad_SGVA;
 
 CREATE TABLE Empresa (
-    CodigoEmpresa INT NOT NULL AUTO_INCREMENT,
+    id_empresa INT NOT NULL AUTO_INCREMENT,
+    CodigoEmpresa INT NOT NULL UNIQUE,
     NombreEmpresa VARCHAR(100) NOT NULL,
-    PRIMARY KEY (CodigoEmpresa)
+    PRIMARY KEY (id_empresa)
 );
 
 CREATE TABLE Trazabilidad (
-    IdTrazabilidad INT NOT NULL AUTO_INCREMENT,
+    IdTrazabilidad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     NombreEmpresa VARCHAR(100) NOT NULL,
     Encargado VARCHAR(100) NOT NULL,
     FechaInicio DATE NOT NULL,
@@ -20,9 +21,36 @@ CREATE TABLE Trazabilidad (
     FechaCierre DATE NULL,
     CodigoEmpresa INT NOT NULL,
     
-    PRIMARY KEY (IdTrazabilidad),
-    
     CONSTRAINT fk_empresa_trazabilidad
     FOREIGN KEY (CodigoEmpresa)
     REFERENCES Empresa (CodigoEmpresa)
 );
+
+ALTER TABLE Trazabilidad
+DROP FOREIGN KEY fk_empresa_trazabilidad;
+
+ALTER TABLE Empresa DROP PRIMARY KEY;
+
+ALTER TABLE Empresa DROP COLUMN id_empresa;
+
+ALTER TABLE Empresa ADD PRIMARY KEY (CodigoEmpresa);
+
+SELECT * FROM Empresa;
+
+DESCRIBE Empresa;
+
+ALTER TABLE Trazabilidad
+CHANGE NombreEmpresa COD_EMPRESA INT;
+
+ALTER TABLE Trazabilidad
+RENAME COLUMN COD_EMPRESA TO cod_empresa;
+
+DESCRIBE Trazabilidad;
+DESCRIBE Empresa;
+
+INSERT INTO Empresa (CodigoEmpresa, NombreEmpresa)
+VALUES (1, 'SENA');
+    
+    
+
+
